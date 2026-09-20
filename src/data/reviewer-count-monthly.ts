6411a7643a222259969c058fe43b14bbd0b80ff7
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2026-09-20 04:39:29 +0000

    Update review counts

--- src/data/reviewer-count-monthly.ts
+++ src/data/reviewer-count-monthly.ts
@@ -7,33 +7,23 @@ export interface ReviewCount {
 }
 export const review_counts_monthly: ReviewCount[] = [
     {
-        url: "https://github.com/Alystrasz",
-        name: "Alystrasz",
-        count: 3,
+        url: "https://github.com/AllusiveWheat",
+        name: "AllusiveWheat",
+        count: 1,
     },
     {
         url: "https://github.com/ASpoonPlaysGames",
         name: "ASpoonPlaysGames",
-        count: 2,
-    },
-    {
-        url: "https://github.com/GeckoEidechse",
-        name: "GeckoEidechse",
-        count: 7,
-    },
-    {
-        url: "https://github.com/NachosChipeados",
-        name: "NachosChipeados",
-        count: 9,
+        count: 1,
     },
     {
         url: "https://github.com/RoyalBlue1",
         name: "RoyalBlue1",
-        count: 3,
+        count: 2,
     },
     {
-        url: "https://github.com/Zanieon",
-        name: "Zanieon",
-        count: 3,
+        url: "https://github.com/sonny-tel",
+        name: "sonny-tel",
+        count: 1,
     },
 ]
